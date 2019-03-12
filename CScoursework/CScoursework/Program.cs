3aee1c3f27effe117f62bb4010add3959ffca6d7
Nichtvren <36418185+Nichtvren@users.noreply.github.com>
2019-03-12 18:02:16 +0000

    Update Program.cs

--- CScoursework/CScoursework/Program.cs
+++ CScoursework/CScoursework/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,27 +10,33 @@ namespace computational_science
 {
     class Program
     {
-        static StreamWriter file = new StreamWriter("Part1.csv");
-        static Random random = new Random();
+        static StreamWriter sw;
+        static StreamReader sr;
+        static Random random;
         static float[] x;
-        static float H = 0.1f;
+        static float H = 0.1f; // step size.
         static float a = -2;
         static float b = 2;
-        static float U = 0;
-        static float Tf = 15;
-        static float T0 = 0;
-        static float n;
-        static float t = 0;
+        static float U = 0; // direction.
+        static float Tf = 15; // final time.
+        static float T0 = 0; // initial time.
+        static float n; 
+        static float t = 0; // time to take.
         static int samples = 0;
-        static float m = 0.0f;
-        static float SD = 0.001f;
-        
+        static float m = 0.0f; // mean.
+        static float SD = 0.001f; // standard deviation.
+        double X = 0.0; // xnoise.
+        double A = 0.0; //inclusive lower bound.
+        double B = 0.0; //inclusive upper bound.
+        static string[] fileData;
 
-        
+
+
 
 
         static void generate()
         {
+            sw = new StreamWriter("Part1.csv");
             n = (Tf - T0) / H;
             x = new float[(int)n + 2];
             x[0] = 1;
@@ -56,8 +62,8 @@ namespace computational_science
                 if (samples == 10)
                 {
                     Console.WriteLine(t + " : " + x[k] + " : " + U);
-                    file.WriteLine(t + "," + x[k] + "," + U);
-                    file.Flush();
+                    sw.WriteLine(t + "," + x[k] + "," + U);
+                    sw.Flush();
                     samples = 0;
                 }           
             }
@@ -65,15 +71,11 @@ namespace computational_science
 
         static void randomness()
         {
-            double x = 0.0;
-            double a = 0.0; //inclusive lower bound.
-            double b = 0.0; //inclusive upper bound.
+            sr = new StreamReader("Part2.csv");
+            random = new Random();
 
-            b = 2 * Math.PI;
 
-            x = random.Next((int)a, (int)b);
-
-           // b = SD - 2
+            
 
         }
 
@@ -82,7 +84,7 @@ namespace computational_science
 
             generate();
 
-            file.Close();       
+            sw.Close();       
             Console.ReadLine();
         }
     }
