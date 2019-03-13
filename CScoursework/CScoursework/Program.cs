9b4f4ca76c824ac2fbe9a2ef29d4215d84e6c5a3
SINEAD SIMPSON <sineadmariesimpson@gmail.com>
2019-03-13 13:06:36 +0000

    did part 2

--- CScoursework/CScoursework/Program.cs
+++ CScoursework/CScoursework/Program.cs
@@ -14,7 +14,7 @@ namespace computational_science
         static StreamReader sr;
         static Random random;
         static float[] x;
-        static float H = 0.1f; // step size.
+        static float H = 0.01f; // step size.
         static float a = -2;
         static float b = 2;
         static float U = 0; // direction.
@@ -24,11 +24,11 @@ namespace computational_science
         static float t = 0; // time to take.
         static int samples = 0;
         static float m = 0.0f; // mean.
-        static float SD = 0.001f; // standard deviation.
-        double X = 0.0; // xnoise.
-        double A = 0.0; //inclusive lower bound.
-        double B = 0.0; //inclusive upper bound.
-        static string[] fileData;
+        static float SD = 0.001f; // standard deviation.       
+        static double A = 0.0; //inclusive lower bound.
+        static double B = 0.0; //inclusive upper bound.
+        static string line;
+        static List<string> generatedData;
 
 
 
@@ -36,50 +36,90 @@ namespace computational_science
 
         static void generate()
         {
-            sw = new StreamWriter("Part1.csv");
-            n = (Tf - T0) / H;
-            x = new float[(int)n + 2];
-            x[0] = 1;
-            for (int k = 0; k < n; ++k)
+            using (sw = new StreamWriter("Part1.txt"))
             {
-                if (t < 5)
+                n = (Tf - T0) / H;
+                x = new float[(int)n + 2];
+                x[0] = 1;
+                for (int k = 0; k < n; ++k)
                 {
-                    U = 2;
-                }
-                if (5 < t && t <= 10)
-                {
-                    U = 1;
-                }
-                if (10 < t && t <= 15)
-                {
-                    U = 3;
-                }
-                x[k + 1] = x[k] + H * (a * x[k] + b * U);
-                t = t + H;
+                    if (t < 5)
+                    {
+                        U = 2;
+                    }
+                    if (5 < t && t <= 10)
+                    {
+                        U = 1;
+                    }
+                    if (10 < t && t <= 15)
+                    {
+                        U = 3;
+                    }
+                    x[k + 1] = x[k] + H * (a * x[k] + b * U);
+                    t = t + H;
 
-                ++samples;
+                    ++samples;
 
-                if (samples == 10)
-                {
-                    Console.WriteLine(t + " : " + x[k] + " : " + U);
-                    sw.WriteLine(t + "," + x[k] + "," + U);
-                    sw.Flush();
-                    samples = 0;
-                }           
+                    if (samples == 10)
+                    {
+                        Console.WriteLine($"{t} : {x[k]} : {U}");
+                        sw.WriteLine($"{t},{x[k]},{U}");
+                        sw.Flush();
+                        samples = 0;
+                    }
+                }
             }
+            
         }
 
         static void randomness()
         {
-            sr = new StreamReader("Part2.csv");
+            sr = new StreamReader("Part1.txt");
+            sw = new StreamWriter("Part2.csv");
             random = new Random();
-            //Read from file line by line into list or something
-            //Separate string to manipulate time
-            //Add randomness 
-            //Print all values back into file
+            generatedData = new List<string>();
+            int iteration = 0; // iteration.
+            double t;
+            double x;
+            double u;
+            double Xn;
+            double z1;
+            double z2 = 0;
 
+            while ((line = sr.ReadLine()) != null)
+            {
+                generatedData.Add(line);
+            }
+            for(int i = 0; i < generatedData.Count; ++i)
+            {
+                string[] line = generatedData[i].Split(new char[] {','});
+                Double.TryParse(line[0], out t);
+                Double.TryParse(line[1], out x);
+                Double.TryParse(line[2], out u);
 
+                if (iteration == 0)
+                {
+                    A = random.NextDouble() * (2 * Math.PI);
+                    B = SD * Math.Sqrt(-2 * Math.Log(random.NextDouble()));
+                    z1 = B * Math.Sin(A) + m;
+                    z2 = B * Math.Cos(A) + m;
+                    Xn = x + z1;
+                    sw.WriteLine($"{t},{x},{Xn},{u}");
+                    sw.Flush();
+                    iteration = 1;
+                } else if (iteration == 1)
+                {
+                    Xn = x + z2;
+                    sw.WriteLine($"{t},{x},{Xn},{u}");
+                    sw.Flush();
+                    iteration = 0;
+                }
+            }
             
+          
+
+           
+
 
         }
 
@@ -87,6 +127,7 @@ namespace computational_science
         {
 
             generate();
+            randomness();
 
             sw.Close();       
             Console.ReadLine();
